08adf3d02c191dd63d3dce1175dc1af5bb50e6c0
krisbal97 <57330402+krisbal97@users.noreply.github.com>
2020-04-12 17:44:12 +0200

    Add files via upload

--- graphForGene.py
+++ graphForGene.py
@@ -9,16 +9,19 @@ import graphviz as gv
 # HPO database is saved in file hpo.txt, this was used to work without internet
 database = open('hpo.txt', 'r')
 graph = obonet.read_obo(database)
-ourData = open("cnvs_with_hpo.tsv", "r")
+ourData = open("cnvs_explicit.tsv", "r")
 id_to_name = {id_: data.get('name') for id_, data in graph.nodes(data = True)}
 
 def findGene(geneName):
+    terms = []
+    expl = ""
     for line in ourData:
         columns = line.split('\t')
         gene = columns[1]
-        if geneName == gene:
-            terms = columns[3].strip().split(";")
-            return terms
+        if geneName == gene and not columns[3]=="-":
+            terms = terms + columns[4].strip().split(";")
+            expl = expl + columns[3].strip() +" "
+    return (expl.strip().split(" "), terms)
 
 class MiniGraph:
 
@@ -28,7 +31,9 @@ class MiniGraph:
             return
         else:
             self.geneName = geneName
-            self.nodes = findGene(geneName)
+            fromData = findGene(geneName)
+            self.nodes = fromData[1]
+            self.explicit = fromData[0]
             self.names = {term: id_to_name[term] for term in self.nodes}
             # in self.miniGraph is wanted subgraph
             self.miniGraph = graph.subgraph(self.nodes)
@@ -37,6 +42,8 @@ class MiniGraph:
             self.impactScore = {node: float(0.0)  for node in self.nodes}
             self.setLabels()
             self.gvGraph.node_attr['color']='#d0f0c0'
+            for term in self.explicit:
+                self.gvGraph.get_node(term).attr['color'] = '#669900'
             self.gvGraph.layout('dot')
 
     def setLabels(self):
@@ -54,15 +61,26 @@ class MiniGraph:
         #we use "copy" of graph1 as template for adding graph2 to it
         self.impactScore = graph1.impactScore.copy()
         graph2copy = graph2.impactScore.copy()
-        colors={node: '#d0f0c0'  for node in graph1.nodes}
+        colors = {}
+        for node in graph1.nodes:
+            if node in graph1.explicit:
+                colors[node]='#669900'
+            else:
+                colors[node]='#d0f0c0'
         for term, score in self.impactScore.items():
             if term in graph2copy:
                 self.impactScore[term] += graph2copy[term]
                 graph2copy.pop(term)
-                colors[term] = '#fceea7'
+                if term in graph2.explicit:
+                    colors[term] = '#ffcc00'
+                else:
+                    colors[term] = '#fceea7'
         for term, score in graph2copy.items():
             self.impactScore[term] = score
-            colors[term] = '#ffb6c1'
+            if term in graph2.explicit:
+                colors[term] = '#ff0066'
+            else:
+                colors[term] = '#ffb6c1'
         self.nodes = self.impactScore.keys()
         self.names = {term: id_to_name[term] for term in self.nodes}
         self.miniGraph = graph.subgraph(self.nodes)
@@ -79,10 +97,9 @@ if len(genes) == 1:
     m = MiniGraph(genes[0])
 else:
     m1 = MiniGraph(genes[0])
-    m2 = MiniGraph(genes[1])
     m1.gvGraph.draw('pokus1.png')
+    m2 = MiniGraph(genes[1])
     m2.gvGraph.draw('pokus2.png')
     m = MiniGraph()
     m.add(m1, m2)
 m.gvGraph.draw('pokus1a2.png')
-
